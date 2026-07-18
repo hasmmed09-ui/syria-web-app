f0bc19ba7e9239c85b960fbc033b412708998b0e
hasmmed09-ui <hasanelmuhammed09@gmail.com>
2026-07-18 14:33:44 +0300

    basta import lazim bide createClient

--- src/lib/supabaseClient.jsx
+++ src/lib/supabaseClient.jsx
@@ -1,4 +1,4 @@
-import {supabase} from "@/lib/supabase/supabase-js";
+import {createClient} from "@/lib/supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
